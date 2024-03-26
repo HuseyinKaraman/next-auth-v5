a5af3bc928c3711ed06d56b1271b29fbfb55805a
Hercules <hsy.krmn@gmail.com>
2024-03-26 18:56:24 +0300

    Work On HomePage

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,34 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export default function Home() {
+  return (
+    <main
+      className="flex flex-col h-full items-center justify-center 
+    bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}>
+          🔐Auth
+        </h1>
+        <p className="text-lg text-white">Next.js + NextAuth</p>
+        <div className="">
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"}>Sign In</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
+}
