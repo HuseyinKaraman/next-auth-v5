8aa55f779f616c3fb9a01b8afe83caa54c8ab583
Hercules <hsy.krmn@gmail.com>
2024-04-04 01:28:36 +0300

    24: Settings page

--- auth.ts
+++ auth.ts
@@ -6,12 +6,14 @@ import { UserRole } from "@prisma/client";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 // https://authjs.dev/getting-started/typescript?frameworks=next#module-augmentation
 declare module "next-auth" {
     interface User {
       role: UserRole;
       isTwoFactorEnabled: boolean;
+      isOAuth: boolean;
     }
 }
 
@@ -69,6 +71,12 @@ export const {
                 session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
             }
 
+            if (session?.user) {
+                session.user.name = token.name;
+                session.user.email = token.email as string;
+                session.user.isOAuth = token.isOAuth as boolean;
+            }
+
             return session;
         },
         async jwt({ token, user }) {
@@ -78,6 +86,11 @@ export const {
 
             if (!existingUser) return token;
 
+            const existingAccount = await getAccountByUserId(existingUser.id);
+
+            token.isOAuth = !!existingAccount;
+            token.name = existingUser.name;
+            token.email = existingUser.email;
             token.role = existingUser.role;
             token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,4 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
 
 
@@ -25,4 +26,32 @@ export const NewPasswordSchema = z.object({
   .refine(data => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
-  })
+})
+
+export const SettingsSchema = z.object({
+  name : z.optional(z.string()),
+  isTwoFactorEnabled: z.optional(z.boolean()),
+  role: z.optional(z.enum([UserRole.ADMIN, UserRole.USER])),
+  email: z.optional(z.string()),
+  password: z.optional(z.string().min(6)),
+  newPassword: z.optional(z.string().min(6)),
+}).refine(data => {
+    if (data.password && !data.newPassword) {
+      return false
+    }
+
+    return true
+  }, {
+    message: "New password is required",
+    path: ["newPassword"],
+  }
+).refine(data => {
+  if (!data.password && data.newPassword) {
+    return false
+  }
+
+  return true
+},{
+  message: "Password is required",
+  path: ["password"],
+});
